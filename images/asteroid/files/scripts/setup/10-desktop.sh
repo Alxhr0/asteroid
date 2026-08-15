178722807b48e3dc2115aaa5f3b1f273e6d201c8
Alxhr0 <alxhr0@proton.me>
2026-08-15 19:54:01 +0200

    Add mariadb for kdepim-addons

--- images/asteroid/files/scripts/setup/10-desktop.sh
+++ images/asteroid/files/scripts/setup/10-desktop.sh
@@ -5,6 +5,8 @@ set -ouex pipefail
 
 pacman --noconfirm -S plasma plasma-login-manager dolphin kate gwenview okular partitionmanager kcalc kcolorchooser ark merkuro kdepim-addons bluez konsole
 
+pacman --noconfirm -S mariadb
+mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
 su builder -c "yay --noconfirm -S klassy kf6-servicemenus-imagetools"
 
