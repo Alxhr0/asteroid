69921ffa402e7f5a6e4645512ee682a9892795af
Alxhr0 <alxhr0@proton.me>
2026-08-15 12:17:32 +0200

    Fix QUALIFIED_KERNEL variable

--- images/asteroid/files/scripts/final/build-initramfs.sh
+++ images/asteroid/files/scripts/final/build-initramfs.sh
@@ -5,7 +5,7 @@ set -eoux pipefail
 echo "::group::Executing build-initramfs"
 trap 'echo "::endgroup::"' EXIT
 
-QUALIFIED_KERNEL="$(pacman -Q linux-cachyos | awk '{print $2}').$(uname -m)"
+QUALIFIED_KERNEL="$(pacman -Q linux-cachyos | awk '{print $2}')-cachyos"
 /usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible --zstd -v --add ostree --add fido2 -f "/usr/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 
 chmod 0600 /usr/lib/modules/"$QUALIFIED_KERNEL"/initramfs.img
