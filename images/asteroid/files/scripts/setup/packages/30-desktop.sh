3711fd1e51ef30f163449fe3098c4926d7c86a99
Alxhr0 <alxhr0@proton.me>
2026-08-19 14:47:54 +0200

    Fix conflict between branding and packages

--- images/asteroid/files/scripts/setup/packages/30-desktop.sh
+++ images/asteroid/files/scripts/setup/packages/30-desktop.sh
@@ -3,6 +3,7 @@
 set -ouex pipefail
 
 
+mv /usr/share/plymouth/themes/spinner/watermark.png /usr/share/plymouth/themes/spinner/watermark.png.bak
 pacman --noconfirm -S plasma plasma-login-manager dolphin kate gwenview okular partitionmanager kcalc kcolorchooser ark merkuro kdepim-addons bluez konsole klassy
 
 pacman --noconfirm -S mariadb
@@ -10,4 +11,6 @@ mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
 su builder -c "yay --noconfirm -S kf6-servicemenus-imagetools"
 
+
+mv /usr/share/plymouth/themes/spinner/watermark.png.bak /usr/share/plymouth/themes/spinner/watermark.png
 systemctl enable plasmalogin
