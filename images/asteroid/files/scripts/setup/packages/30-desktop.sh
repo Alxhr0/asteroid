81ca0d319527a83c1e00c045f6fc0a2eadec440e
Alxhr0 <alxhr0@proton.me>
2026-08-27 21:32:21 +0200

    Try Klassy from the AUR

--- images/asteroid/files/scripts/setup/packages/30-desktop.sh
+++ images/asteroid/files/scripts/setup/packages/30-desktop.sh
@@ -4,12 +4,12 @@ set -ouex pipefail
 
 
 mv /usr/share/plymouth/themes/spinner/watermark.png /usr/share/plymouth/themes/spinner/watermark.png.bak
-pacman --noconfirm -S plasma plasma-login-manager dolphin kate gwenview okular partitionmanager kcalc kcolorchooser ark merkuro kdepim-addons bluez konsole klassy
+pacman --noconfirm -S plasma plasma-login-manager dolphin kate gwenview okular partitionmanager kcalc kcolorchooser ark merkuro kdepim-addons bluez konsole
 
 pacman --noconfirm -S mariadb
 mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
-su builder -c "yay --noconfirm -S kf6-servicemenus-imagetools"
+su builder -c "yay --noconfirm -S klassy kf6-servicemenus-imagetools"
 
 pacman --noconfirm -R plasma-bigscreen
 
