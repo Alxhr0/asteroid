2d779930bd0dee3d3c2331f780df486e1969601a
Alxhr0 <alxhr0@proton.me>
2026-08-29 02:41:04 +0200

    Remove fix for pear-desktop

--- images/asteroid/files/scripts/final/workarounds.sh
+++ images/asteroid/files/scripts/final/workarounds.sh
@@ -3,7 +3,7 @@
 set -eoux pipefail
 
 ## Fix Youtube Music
-sed -i 's|/opt/YouTube\\ Music/youtube-music|/usr/share/factory/opt/YouTube\\ Music/youtube-music|' /usr/bin/youtube-music
+#sed -i 's|/opt/YouTube\\ Music/youtube-music|/usr/share/factory/opt/YouTube\\ Music/youtube-music|' /usr/bin/youtube-music
 
 # Fix Megasync
 mv /usr/bin/megasync /opt/megasync/megasync
